30a4902fd012bc11cb48328a09cb7dd4b4050126
Avik Dangar <123013809+MyselfAvik@users.noreply.github.com>
2025-08-23 13:54:53 +0530

    cors error fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://expense-tracker-mu-vert.vercel.app",
     credentials: true,
   })
 );
